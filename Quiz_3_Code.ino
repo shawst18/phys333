9999725a70262aeb52a417cfe64a9dacb479ca0d
shawst18 <60192797+shawst18@users.noreply.github.com>
2020-04-02 12:40:48 -0500

    Update Quiz_3_Code.ino

--- Quiz_3_Code.ino
+++ Quiz_3_Code.ino
@@ -1,3 +1,4 @@
+//program for reading an LM34 Temp sensor using an arduino
 
 unsigned long t1;
 int Sensvalue;
@@ -9,10 +10,10 @@ void setup() {
 }
 
 void loop() {
-  t1 = millis();
-  Sensvalue = analogRead(A0);
-  voltage = (float)Sensvalue * (float)(5.0 / 1023.0);
-  temp = voltage * 100;
+  t1 = millis();  //Get time
+  Sensvalue = analogRead(A0); //Get analog read value [0, 1023]
+  voltage = (float)Sensvalue * (float)(5.0 / 1023.0); //Convert analog read value to voltage [0, 5]volts
+  temp = voltage * 100; //Convert to temp in F., 10mv per degree F.
   Serial.print(t1);
   Serial.print(",\t");
   Serial.print(Sensvalue);
